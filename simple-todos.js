69502b78f70dba9d157864e467e9e73c11b8db05
Matt McInnis <mcinnis.matt@gmail.com>
2015-07-25 12:00:39 +0200

    tutorial app

---
+++ simple-todos.js
@@ -0,0 +1,44 @@
+Tasks = new Mongo.Collection("tasks");
+ 
+if (Meteor.isClient) {
+  // This code only runs on the client
+  Template.body.helpers({
+    tasks: function () {
+      // Show newest tasks at the top
+      return Tasks.find({}, {sort: {createdAt: -1}});
+    }
+  });
+
+  Template.body.events({
+      "submit .new-task": function (event) {
+        // Prevent default browser form submit
+        event.preventDefault();
+        console.log(event);
+        // Get value from form element
+        var text = event.target.text.value;
+   
+        // Insert a task into the collection
+        Tasks.insert({
+          text: text,
+          createdAt: new Date() // current time
+        });
+   
+        // Clear form
+        event.target.text.value = "";
+      }
+
+
+    });
+
+  Template.task.events({
+      "click .toggle-checked": function () {
+        // Set the checked property to the opposite of its current value
+        Tasks.update(this._id, {
+          $set: {checked: ! this.checked}
+        });
+      },
+      "click .delete": function () {
+        Tasks.remove(this._id);
+      }
+    });
+}
