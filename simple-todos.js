d83d409ae7c24bb4da574fbc2181b0a5e5aa6b86
Matt McInnis <mcinnis.matt@gmail.com>
2015-07-25 12:15:44 +0200

    update

--- simple-todos.js
+++ simple-todos.js
@@ -1,11 +1,27 @@
 Tasks = new Mongo.Collection("tasks");
  
 if (Meteor.isClient) {
+
+
+
+
+
+
+
+
   // This code only runs on the client
   Template.body.helpers({
     tasks: function () {
       // Show newest tasks at the top
       return Tasks.find({}, {sort: {createdAt: -1}});
+    },
+    form: function() {
+      HTTP.call(
+        "POST", 
+        "https://api.typeform.io/latest/forms", 
+        { "headers": 
+          {"X-API-TOKEN":"d4400a7768341dafbee86b8628aa26f3"}
+      });
     }
   });
 
